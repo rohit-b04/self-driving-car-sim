c9445831bddf641593cd134be119be92d62f4bd7
rohit-b04 <rohitbargal0@gmail.com>
2025-08-17 14:36:50 +0530

    Enhance car simulation with damage assessment and sensor integration

--- car.js
+++ car.js
@@ -7,15 +7,56 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = 5;
         this.friction = 0.05;
         this.angle = 0;
+        this.damaged = false;
+        this.polygon = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls ()
     }
-    update() {
-        this.#move();
+    update(roadBorders) {
+        if(!this.damaged) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders);
+        }
+            
+        this.sensor.update(roadBorders);
     }
+
+
+    #assessDamage(roadBorders) {
+        for(let i = 0;i < roadBorders.length;i++) {
+            if(polysIntersect(this.polygon, roadBorders[i])) return true;
+        }
+        return false;
+    }
+
+    #createPolygon() {
+        const points=[];
+        const rad = Math.hypot(this.width, this.height)/2;
+        const alpha = Math.atan2(this.width, this.height)
+        points.push({
+            x: this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        })
+        points.push({
+            x: this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        })
+        points.push({
+            x: this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        })
+        points.push({
+            x: this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        })
+        return points;
+    }
+
     #move() {
         if(this.controls.forward) {
             this.speed += this.acceleration
@@ -47,17 +88,17 @@ class Car {
         this.y -= Math.cos(this.angle)*this.speed;
     }
     draw(ctx) {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle)
-        ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        )
+
+        if(this.damaged) ctx.fillStyle="gray";
+        else ctx.fillStyle="black";
+
+        ctx.beginPath()
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for(let i = 0;i < this.polygon.length;i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
-        ctx.restore();
+
+        this.sensor.draw(ctx)
     }
 }
--- utils.js
+++ utils.js
@@ -1,3 +1,58 @@
-function lerp(A, B, t){
+function lerp(A, B, t){ 
+    // this lerp function returns the point at a specific coordinate according to the t which is percentage in AB segment
+    // this is called linear interpolation
     return A+(B-A)*t;
+}
+
+function getIntersection(A,B,C,D) {
+
+    /*
+        Ix = Ax+(Bx-Ax)t = Cx+(Dx-Cx)u
+        Iy = Ay+(By-Ay)t = Cy+(Dy-Dy)u
+
+        1. Ax+(Bx-Ax)t = Cx+(Dx-Cx)u
+            (Ax-Cx)+(Bx-Ax)t = (Dx-Cx)u
+
+
+        2. Ay+(By-Ay)t = Cy+(Dy-Cy)u
+            (Ay-Cy)+(By-Ay)t = (Dy-Cy)u
+
+        After some derivation we get the value of t
+        
+     */
+    const tNumerator=(D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x)
+    const denominator=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y)
+
+    const uNumerator=(C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y)
+
+
+    if(denominator != 0) {
+        const t = tNumerator/denominator;
+        const u = uNumerator/denominator
+        if(t >= 0 && t <= 1 && u>=0 && u <= 1) {
+            return {
+                x:lerp(A.x, B.x,t),
+                y:lerp(A.y, B.y, t),
+                offset: t
+            }
+        }
+        
+    }
+    return null
+
+}
+
+function polysIntersect(poly1, poly2) {
+    for(let i = 0;i < poly1.length;i++) {
+        for(let j = 0;j < poly2.length;j++) {
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if(touch) return true;
+        }
+    }
+    return false;
 }
