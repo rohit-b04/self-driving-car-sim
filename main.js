d666ea799f7bf9a1481a8b2c96ffe7a5d46ec999
rohit-b04 <rohitbargal0@gmail.com>
2025-08-18 11:49:33 +0530

    Implement neural network for AI control in car simulation; update canvas elements and styles for visualization

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
-const canvas = document.getElementById("myCanvas")
+const carCanvas = document.getElementById("carCanvas")
 
-canvas.width = 200
+carCanvas.width = 200
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2, canvas.width*0.9)
-const car = new Car(road.getLaneCenter(3), 100, 30, 50, "KEYS","blue");
+const networkCanvas = document.getElementById("networkCanvas")
+
+networkCanvas.width = 300
+
+const carCtx = carCanvas.getContext("2d");
+
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width/2, carCanvas.width*0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI","blue");
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY","red",3)
 ]
 
 
 animate()
-function animate() {
+function animate(time) {
     for(let i = 0;i < traffic.length;i++) {
         traffic[i].update(road.borders, []);
     }
     car.update(road.borders, traffic);
-    canvas.height = window.innerHeight;
-    ctx.save();
-    ctx.translate(0, -car.y+canvas.height*0.7);
-    road.draw(ctx);
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
+    carCtx.save();
+    carCtx.translate(0, -car.y+carCanvas.height*0.7);
+    road.draw(carCtx);
 
     
-    for(let i = 0;i < traffic.length;i++) traffic[i].draw(ctx);
-    car.draw(ctx);
-    ctx.restore();
+    for(let i = 0;i < traffic.length;i++) traffic[i].draw(carCtx);
+    car.draw(carCtx);
+    carCtx.restore();
+    networkCtx.lineDashOffset = -time/50;
+    Visualizer.drawNetwork(networkCtx, car.brain);
+    
+
     requestAnimationFrame(animate); // requestAnimateFrame calls the animate method again and again many times we want, it gives the illusion of movement
 }
  
